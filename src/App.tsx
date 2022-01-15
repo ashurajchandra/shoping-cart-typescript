5489ebd3748a60234ac6cf05f971a7dd625daeb2
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-01-15 15:53:16 +0530

    handled addto cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,21 @@ const App=()=> {
   console.log(data);
   const getTotalItems = (items: CartItemType[])=>
   items.reduce((totalAmountItem:number, item)=>totalAmountItem + item.amount,0);
-  const handleAddToCart = (clickedItem:CartItemType)=> null;
+  const handleAddToCart = (clickedItem:CartItemType)=> {
+    setCartItems(prev =>{
+      //to check if the item is already present in the cart
+      const isItemInCart = prev.find(item=>item.id === clickedItem.id)
+      if(isItemInCart) {
+        return prev.map(item =>
+          item.id === clickedItem.id
+          ?{...item, amount: item.amount + 1}
+          : item
+          );
+      };
+      //first time the item is added
+      return [...prev, {...clickedItem, amount:1}]
+    })
+  };
   const handleRemoveFromCart = ()=> null;
 
    if(isLoading){
